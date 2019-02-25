78bc69a4cfb3b3be2d44abd9bb93152693260f99
Arnab Chanda <arnabchanda964@gmail.com>
2019-02-26 01:15:21 +0530

    generated the results of initial test.Model working fine with apriori!

--- CaseStudy/GradesToMarks.py
+++ CaseStudy/GradesToMarks.py
@@ -8,59 +8,12 @@ marks = {'O':10,'E':9,'A':8,'B':7,'C':6,'D':5,'F':4}
 dataFrame.drop(columns = 'Sl')
 dataFrame.set_index(' ROLL NO. ',inplace  = True)
 
-x = 0
-for i in dataFrame["HU101"]:
-    dataFrame["HU101"][x:x+1]=dataFrame["HU101"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
+subjectList = ['HU101','PH101','M101','ME101','ES101','PH191','ES191','ME192','HU181','XC181']
+for subjects in subjectList:
+    x = 0
+    for i in dataFrame[subjects]:
+        dataFrame[subjects][x:x+1]=dataFrame[subjects][x:x+1].str.replace(i,str(marks[i]))
+        x = x+1
 
-x = 0
-for i in dataFrame["PH101"]:
-    dataFrame["PH101"][x:x+1]=dataFrame["PH101"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["M101"]:
-    dataFrame["M101"][x:x+1]=dataFrame["M101"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["ME101"]:
-    dataFrame["ME101"][x:x+1]=dataFrame["ME101"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["ES101"]:
-    dataFrame["ES101"][x:x+1]=dataFrame["ES101"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["PH191"]:
-    dataFrame["PH191"][x:x+1]=dataFrame["PH191"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["ES191"]:
-    dataFrame["ES191"][x:x+1]=dataFrame["ES191"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["ME192"]:
-    dataFrame["ME192"][x:x+1]=dataFrame["ME192"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["HU181"]:
-    dataFrame["HU181"][x:x+1]=dataFrame["HU181"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-x = 0
-for i in dataFrame["XC181"]:
-    dataFrame["XC181"][x:x+1]=dataFrame["XC181"][x:x+1].str.replace(i,str(marks[i]))
-    x = x+1
-
-
-# for elements in dataFrame.index:
-#     print(dataFrame.loc[10900113021])
-
-dataFrame.to_csv(os.path.join(sys.path[0],'datanew.csv'))
+dataFrame.to_csv(os.path.join(sys.path[0],'data_new.csv'))
 #print(dataFrame.loc[10900113021])
